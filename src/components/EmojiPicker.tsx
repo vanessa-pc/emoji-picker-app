19542a4eaf4719938e58109b309cf1b2f00bfefe
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-08-24 17:27:20 +0300

    Run format, lint, test checks

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -1,31 +1,32 @@
-import {useState} from 'react';
-import Emojis from './Emojis';
+import { useState } from "react";
+import Emojis from "./Emojis";
 // import './Emoji.css'
 
 export default function EmojiPicker(): JSX.Element {
-    
-    const [pickedEmoji, setPickedEmoji] =
-    useState("");
+  const [pickedEmoji, setPickedEmoji] = useState("");
 
-    const [lastEmoji, setLastEmoji] =
-    useState<string[]>([]);
+  const [lastEmoji, setLastEmoji] = useState<string[]>([]);
 
   const handlePickedEmoji = (emoji: string) => {
     setPickedEmoji(emoji);
     setLastEmoji((emojis) => [...emojis, emoji]);
   };
 
-
-    return (
-      <>
-        <h1>Emoji picker</h1>
-        <p>Please pick an emoji from below:</p>
-        <div>
-        {Emojis.map(function (emoji,index) {
-        return <button onClick={() =>handlePickedEmoji(emoji)} key={index} >{emoji}</button>;})}
-        </div>
-        <p>You picked: {pickedEmoji}</p>
-        <p>Your previously picked emojis: {lastEmoji}</p>
-      </>
-    );
+  return (
+    <>
+      <h1>Emoji picker</h1>
+      <p>Please pick an emoji from below:</p>
+      <div>
+        {Emojis.map(function (emoji, index) {
+          return (
+            <button onClick={() => handlePickedEmoji(emoji)} key={index}>
+              {emoji}
+            </button>
+          );
+        })}
+      </div>
+      <p>You picked: {pickedEmoji}</p>
+      <p>Your previously picked emojis: {lastEmoji}</p>
+    </>
+  );
 }
